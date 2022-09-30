237f737ad10994ccf48e4d77d86d7fea0d97a7f2
Yannick Uhlmann <augustunderground@protonmail.com>
2022-09-30 10:19:41 +0200

    handle 1 and n number of sessions dynamically in module

--- pyspectre/__init__.py
+++ pyspectre/__init__.py
@@ -1,6 +1,105 @@
 """ Python interface for Cadence Spectre """
 
-from .pyspectre import Session, start_session, stop_session, \
-                       run_all, run_analysis, get_analyses, \
-                       get_parameters, set_parameters, \
-                       simulate, simulate_netlist
+from collections.abc import Iterable
+from typing import Callable, Union
+from multiprocessing import cpu_count
+from multiprocessing.dummy import Pool
+import pandas as pd
+import pynut as pn
+from . import core
+
+def _run(num: int, fun: Callable, *args):
+    """
+    Run function in parallel or not
+    """
+    num_par = min(num, cpu_count())
+    if num > 1:
+        with Pool(num_par) as pool:
+            res = pool.starmap(fun, zip(*args))
+    else:
+        res = fun(*args)
+    return res
+
+def simulate( net_path: Union[str,Iterable[str]]
+            , includes: Union[Iterable[str], Iterable[Iterable[str]]] = None
+            , raw_path: Union[str,Iterable[str]] = None
+            ) -> Union[pn.NutMeg, Iterable[pn.NutMeg]]:
+    """
+    Passes the given netlists to spectre instances and reads the results in.
+    """
+    num = len(net_path) if isinstance(net_path, Iterable) else 1
+    return _run(num, core.simulate, net_path, includes, raw_path)
+
+def simulate_netlists( netlist: Union[str,Iterable[str]]
+                     , includes: Union[Iterable[str], Iterable[Iterable[str]]] = None
+                     , raw_path: Union[str,Iterable[str]] = None
+                     ) -> Union[pn.NutMeg, Iterable[pn.NutMeg]]:
+    """
+    Passes the given netlists to spectre instances and reads the results in.
+    """
+    num = len(netlist) if isinstance(netlist, Iterable) else 1
+    return _run(num, core.simulate_netlist, netlist, includes, raw_path)
+
+def start_session( net_path: Union[str, Iterable[str]]
+                 , includes: Union[Iterable[str], Iterable[Iterable[str]]] = None
+                 , raw_path: Union[str, Iterable[str]] = None
+                 ) -> Union[core.Session, Iterable[core.Session]]:
+    """
+    Start spectre interactive session(s)
+    """
+    num = len(net_path) if isinstance(net_path, Iterable) and not isinstance(net_path, str) else 1
+    raws = num * [raw_path] if num > 1 else raw_path
+    return _run(num, core.start_session, net_path, includes, raws)
+
+def run_all( session: Union[core.Session, Iterable[core.Session]]
+           ) -> Union[ dict[str, pd.DataFrame]
+                     , Iterable[dict[str, pd.DataFrame]] ]:
+    """
+    Run all simulation analyses
+    """
+    num = 1 if isinstance(session, core.Session) else len(session) 
+    return _run(num, core.run_all, session)
+
+def get_analyses( session: core.Session
+                ) -> Union[dict[str, str], Iterable[dict[str, str]]]:
+    """
+    Retrieve all simulation analyses from current netlist
+    """
+    num = 1 if isinstance(session, core.Session) else len(session)
+    return _run(num, core.get_analyses, session)
+
+def run_analysis( session: Union[core.Session, Iterable[core.Session]]
+                , analysis: Union[str, Iterable[str]]
+                ) -> Union[ dict[str, pd.DataFrame]
+                          , Iterable[dict[str, pd.DataFrame]] ]:
+    """
+    Run only the given analysis.
+    """
+    num = 1 if isinstance(session, core.Session) else len(session)
+    return _run(num, core.run_analysis, session, analysis)
+
+def set_parameters( session: Union[core.Session, Iterable[core.Session]]
+                  , params: Union[dict[str, float], Iterable[dict[str, float]]]
+                  ) -> Union[bool, Iterable[bool]]:
+    """
+    Set a list of parameters
+    """
+    num = 1 if isinstance(session, core.Session) else len(session)
+    return _run(num, core.set_parameters, session, params)
+
+def get_parameters( session: Union[core.Session, Iterable[core.Session]]
+                  , params: Union[Iterable[str], Iterable[Iterable[str]]]
+                  ) -> Union[dict[str, float], Iterable[dict[str, float]]]:
+    """
+    Get a parameter in the netlist.
+    """
+    num = 1 if isinstance(session, core.Session) else len(session)
+    return _run(num, core.get_parameters, session, params)
+
+def stop_session( session: Union[core.Session,Iterable[core.Session]]
+                ) -> Union[bool, Iterable[bool]]:
+    """
+    Stop spectre interactive session(s)
+    """
+    num = 1 if isinstance(session, core.Session) else len(session)
+    return _run(num, core.stop_session, session)
