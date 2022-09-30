fb95ff6077fff2f81f04260538a6241ba734c4a0
Yannick Uhlmann <augustunderground@protonmail.com>
2022-09-30 10:19:57 +0200

    1 and 5 parallel sessions

--- test/example.py
+++ test/example.py
@@ -1,12 +1,17 @@
-import pyspectre.pool as ps
+import pyspectre as ps
 import pandas as pd
 import numpy as np
 
-netlist  = '/home/uhlmanny/.ace/xh035-3V3/op2/mcg_300.scs'
-includes = ['/home/uhlmanny/.ace/xh035-3V3/pdk']
+netlist  = '~/.ace/xh035-3V3/op2/mcg_300.scs'
+includes = ['~/.ace/xh035-3V3/pdk']
+
+## Single Process
+session  = ps.start_session(netlist, includes)
+analyses = ps.get_analyses(session)
+results  = ps.run_all(session)
 
 ## Single Process
 n        = 5
-sessions = ps.start_n_sessions(netlist, n, includes)
+sessions = ps.start_session(n * [netlist], n * [includes])
 analyses = ps.get_analyses(sessions)
 results  = ps.run_all(sessions)
