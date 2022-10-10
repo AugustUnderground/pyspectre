5fc8db570053d2fbf8db9fb6a0ef520024527654
Yannick Uhlmann <augustunderground@protonmail.com>
2022-10-10 16:18:26 +0200

    better example

--- test/example.py
+++ test/example.py
@@ -2,8 +2,8 @@ import pyspectre as ps
 import pandas as pd
 import numpy as np
 
-netlist  = '~/.ace/xh035-3V3/op2/mcg_300.scs'
-includes = ['~/.ace/xh035-3V3/pdk']
+netlist = './test/example.scs'
+includes = ['/opt/pdk/gpdk180_v3.3/models/spectre']
 
 ## One-Shot
 results = ps.simulate(netlist, includes)
@@ -11,14 +11,14 @@ results = ps.simulate(netlist, includes)
 ## Single Process
 session  = ps.start_session(netlist, includes)
 analyses = ps.get_analyses(session)
-params   = ps.get_parameters(session, ['Wcm2', 'Ld'])
-ps.set_parameters(session, {'Wcm2': 2.0e-6, 'Ld': 1.0e-6})
+params   = ps.get_parameters(session, ['Wcm2', 'Ldp1'])
+ps.set_parameters(session, {'Wcm2': 1.5e-6, 'Ldp1': 1.0e-6})
 results  = ps.run_all(session)
 
 ## Parallel Jobs
 n        = 5
 sessions = ps.start_session(n * [netlist], n * [includes])
 analyses = ps.get_analyses(sessions)
-params   = ps.get_parameters(sessions, n * [['Wcm2', 'Ld']])
-ps.set_parameters(sessions, n * [{'Wcm2': 2.0e-6, 'Ld': 1.0e-6}])
+params   = ps.get_parameters(sessions, n * [['Wcm2', 'Ldp1']])
+ps.set_parameters(sessions, n * [{'Wcm2': 2.0e-6, 'Ldp1': 1.0e-6}])
 results  = ps.run_all(sessions)
